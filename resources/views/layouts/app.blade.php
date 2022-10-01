3b5ed5572c5c4ea16d8f7c48ca878b464c56c8ad
priya6589 <priyatripathi6589>
2022-10-01 18:41:26 +0530

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,195 +1,80 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Dashboard</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="bower_components/morris.js/morris.css">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-  <!-- Google Font -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-  @stack('header_extras')
-</head>
-<body>
-@include('layouts.loader')
-@include('layouts.loader')
-    @stack('scripts')
-</body>
-</html>
-
-@php
-$user = Auth::user();
-@endphp
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>HACHMAN DASHBOARD</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/logo.png') }}">
-    <link href="{{ asset('plugins/pg-calendar/css/pignose.calendar.min.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('plugins/chartist/css/chartist.min.css') }}">
-    <link rel=" stylesheet" href="{{ asset('plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css') }}">
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/bootstrap-tagsinput.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/amsify.suggestags.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/select2.css') }}" rel="stylesheet" />
-    <link href="{{ asset('css/dropzone.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('css/sweetalert.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('css/datatable.css') }}" rel="stylesheet" />
-    <style>
-        .ck {
-            height: 100px;
-        }
 
-        .select2-container {
-            width: 100% !important;
-            box-sizing: border-box;
-            display: inline-block;
-            margin: 0;
-            position: relative;
-            vertical-align: middle;
-        }
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    </style>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    @error('plan_desc')
-        <style>
-            .ck {
-                height: 100px;
-
-            }
-            .ck-content {
-                border-color: #dc3545 !important;
-                padding-right: calc(1.5em + .75rem) !important;
-                background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12' width='12' height='12' fill='none' stroke='%23dc3545'%3e%3ccircle cx='6' cy='6' r='4.5'/%3e%3cpath stroke-linejoin='round' d='M5.8 3.6h.4L6 6.5z'/%3e%3ccircle cx='6' cy='8.2' r='.6' fill='%23dc3545' stroke='none'/%3e%3c/svg%3e") !important;
-                background-repeat: no-repeat !important;
-                background-position: right calc(.375em + .1875rem) center !important;
-                background-size: calc(.75em + .375rem) calc(.75em + .375rem) !important;
-            }
-
-        </style>
-    @enderror
-
-    @error('plan_task[]')
-        <style>
-            .select2-container--default .select2-selection--multiple {
-                border-color: #dc3545 !important;
-                box-sizing: border-box !important;
-                cursor: pointer !important;
-                display: block !important;
-                min-height: 32px !important;
-                user-select: none !important;
-                -webkit-user-select: none !important;
-                padding-right: calc(1.5em + 0.75rem);
-                background-image: url(data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12' width='12' height='12' fill='none' stroke='%23dc3545'%3e%3ccircle cx='6' cy='6' r='4.5'/%3e%3cpath stroke-linejoin='round' d='M5.8 3.6h.4L6 6.5z'/%3e%3ccircle cx='6' cy='8.2' r='.6' fill='%23dc3545' stroke='none'/%3e%3c/svg%3e);
-                background-repeat: no-repeat;
-                background-position: right calc(0.375em + 0.1875rem) center;
-                background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);
-            }
-
-        </style>
-    @enderror
-    @stack('header_extras')
-
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-
-
-
 <body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
 
-    <!--**********************************
-        Main wrapper start
-    ***********************************-->
-    <div id="main-wrapper">
+                    </ul>
 
-        @include('layouts.loader')
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
 
-        @include('layouts.header')
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
 
-        @if($user->role == "admin")
-            @include('layouts.sidebar');
-        @elseif($user->role == "staff")
-            @include('layouts.staffsidebar');
-        @else
-            @include('layouts.usersidebar');
-        @endif
-        
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-        <div class="content-body">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-            @include('layouts.alerts')
-
-            @yield('content')
-
-        </div>
-
-        @include('layouts.footer')
-
+        <main class="py-4">
+            {{-- @yield('content') --}}
+        </main>
     </div>
-
-    <!--**********************************
-        Scripts
-    ***********************************-->
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('plugins/common/common.min.js') }}"></script>
-    <script src="{{ asset('js/validate.js') }}"></script>
-    <script src="{{ asset('js/datatable.js') }}"></script>
-
-    <script src="{{ asset('js/custom.min.js') }}"></script>
-    <script src="{{ asset('js/settings.js') }}"></script>
-    <script src="{{ asset('js/gleek.js') }}"></script>
-    <script src="{{ asset('js/styleSwitcher.js') }}"></script>
-        
-    
-    <!-- Pignose Calender -->
-    <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
-    <script src="{{ asset('plugins/pg-calendar/js/pignose.calendar.min.js') }}"></script>
-    <!-- ChartistJS -->    
-    <script src="{{ asset('js/dashboard/dashboard-1.js') }}"></script>
-    <script src="{{ asset('js/bootstrap-tagsinput.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.amsify.suggestags.js') }}"></script>
-    <script src="{{ asset('js/custom-backend/custom.js') }}"></script>
-    <script src="{{ asset('js/bundle.min.js') }}"></script>
-    <script src="{{ asset('js/select2.min.js') }}"></script>
-    <script src="{{ asset('js/dropzone.min.js') }}"></script>
-    <script src="{{ asset('js/sweetalert.min.js') }}"></script>
-    <script src="https://kit.fontawesome.com/bbf6d5aab6.js" crossorigin="anonymous"></script>
-    @stack('scripts')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function() {
+    return view('auth.login'); });
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
