28c9051261be8ed5d462da97cdbcc98acdae9618
priya6589 <priyatripathi6589>
2022-10-03 18:15:42 +0530

    committed

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function() {
     return view('auth.login'); });
 
+
+    Route::get('/ins', function() {
+        return view('layouts.inspector.dashboard_inspector'); });
+
+    Route::get('/agency', function() {
+       return view('layouts.company.dashboard_company'); });
+
+    Route::get('/agency-register',[App\Http\Controllers\UserController::class, 'index'])->name('agency-user');
+
+
 Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
