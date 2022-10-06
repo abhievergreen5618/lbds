d59d68184cf0e7f61d8baf36bd90765b6b1fb2b4
abhi5618 <abhishek@evergreenbrain.com>
2022-10-07 01:30:33 +0530

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -75,44 +75,18 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'company_name' => $data['company_name'],
+            'city' => $data['city'],
+            'zip_code' => $data['zip_code'],
+            'company_phonenumber' => $data['company_phonenumber'],
+            'email' => $data['email'],
+            'email' => $data['email'],
+            'role' => "3",
             'password' => Hash::make($data['password']),
         ]);
-    }
-    public function store(Request $request, User $user)
-    {
-        $request->validate([
-            'company_name'                => 'required',
-            'company_address'             => 'required',
-            'city'                        => 'required|max:11',
-            'zip_code'                    => 'required|max:11',
-            'company_phonenumber'         => 'required',
-            'name'                        => 'required',
-            'direct_number'               => 'required',
-            'email'                       => 'required|unique:users|max:255',
-            'password'                    => 'required|min:6',
-        ],
-        [
-            "required" => "Field is required."
-        ]
-    );
-
-        $user                       = new User();
-        $user->company_name         =$request->company_name;
-        $user->company_address      =$request->company_address;
-        $user->city                 =$request->city;
-        $user->zip_code             =$request->zip_code;
-        $user->company_phonenumber  =$request->company_phonenumber;
-        $user->name                 =$request->name;
-        $user->direct_number        =$request->direct_number;
-        $user->email                =$request->email;
-        $user->password             =Hash::make($request->password);
-        $user->role                 ='3';
-        $user->save();
-
         return redirect()->route('login')->with(["msg"=>"Registered Successfully"]);
-
     }
 }
