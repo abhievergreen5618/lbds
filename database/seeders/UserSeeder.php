862a27d45b7afdbf785a5e5d47442bc37dfc52eb
abhi5618 <abhishek@evergreenbrain.com>
2022-10-17 18:57:45 +0530

    new admin user created

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -23,15 +24,24 @@ class UserSeeder extends Seeder
             "role" => "1",
         ]);
         User::create([
+            "name" => "Bishoples",
+            "email" => "bishoples@gmail.com",
+            "password" => Hash::make("12345678"),
+            "email_verified_at" => Carbon::now()->timestamp,
+            "role" => "1",
+        ]);
+        User::create([
             "name" => "inspector",
             "email" => "testinspector@gmail.com",
             "password" => Hash::make("12345678"),
+            "email_verified_at" => Carbon::now()->timestamp,
             "role" => "2",
         ]);
         User::create([
             "name" => "company",
             "email" => "testcompany@gmail.com",
             "company_name" => "Wind Mitigation",
+            "email_verified_at" => Carbon::now()->timestamp,
             "password" => Hash::make("12345678"),
             "role" => "3",
         ]);
