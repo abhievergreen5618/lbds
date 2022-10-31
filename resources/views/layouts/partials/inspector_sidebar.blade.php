2abaf1938cd7978ceb4c3d240bffdccd4096fc9a
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2022-11-01 00:09:48 +0530

    logo update sidebar

--- resources/views/layouts/partials/inspector_sidebar.blade.php
+++ resources/views/layouts/partials/inspector_sidebar.blade.php
@@ -1,10 +1,9 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-            style="opacity: .8">
-        <span class="brand-text font-weight-light">WINDMITIGATION</span>
+    <a href="{{route('home')}}" class="brand-link">
+        <img src="{{ asset('public/images/logo.png') }}" alt="Logo" class="elevation-3" style="max-height: 28px !important;">
+        {{-- <span class="brand-text font-weight-light">WINDMITIGATION</span> --}}
     </a>
 
     <!-- Sidebar -->
@@ -15,7 +14,7 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div> --}}
             <div class="info">
-                <a href="#" class="d-block">{{ ucfirst(Auth::user()->name) }}</a>
+                <a href="{{route('home')}}" class="d-block">{{ ucfirst(Auth::user()->name) }}</a>
             </div>
         </div>
 
