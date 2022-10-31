48f5cecc0aee27a9fc016b77dca5d870f934417a
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2022-10-31 23:08:29 +0530

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{ asset('dist/img/black.png') }}" style="max-height: 100% !important; width:100%!important;" class="logo" alt="Laravel Logo" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; max-width: 100%; border: none; height: 75px; max-height: 75px; width: 75px;">
+<img src="{{ asset('public/images/black.png') }}" style="max-height: 100% !important; width:100%!important;" class="logo" alt="Laravel Logo" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; max-width: 100%; border: none; height: 75px; max-height: 75px; width: 75px;">
 @else
 <img src="{{asset('dist/img/AdminLTELogo.png')}}" class="logo" alt="My logo">
 @endif
