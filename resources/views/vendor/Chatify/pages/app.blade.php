4cee3c7f7631674cb34d96741241ae645e425199
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2022-11-01 22:33:36 +0530

    update

--- resources/views/vendor/Chatify/pages/app.blade.php
+++ resources/views/vendor/Chatify/pages/app.blade.php
@@ -71,7 +71,8 @@
                 {{-- header back button, avatar and user name --}}
                 <div class="chatify-d-flex chatify-justify-content-between chatify-align-items-center">
                     <a href="#" class="show-listView"><i class="fas fa-arrow-left"></i></a>
-                    <div class="avatar av-s header-avatar" style="margin: 0px 10px; margin-top: -5px; margin-bottom: -5px;">
+                    @php $imglink = (!empty(Auth::user()->profile_img)) ? Auth::user()->profile_img : asset('images/profile/profile.jpg'); @endphp
+                    <div class="avatar av-s header-avatar" style="margin: 0px 10px; margin-top: -5px; margin-bottom: -5px; background-image:url('asset('$imglink')')">
                     </div>
                     <a href="#" class="user-name">{{ config('chatify.name') }}</a>
                 </div>
