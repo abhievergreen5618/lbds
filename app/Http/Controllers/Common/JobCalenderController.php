1a14b117e9843b2f3bcdc24a2270c7e5ec13b907
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2022-11-01 22:55:20 +0530

    update

--- app/Http/Controllers/Common/JobCalenderController.php
+++ app/Http/Controllers/Common/JobCalenderController.php
@@ -40,7 +40,8 @@ class JobCalenderController extends Controller
                     $returnarr[$key]['city'] = $value->city;
                     $returnarr[$key]['zipcode'] = $value->zipcode;
                     $returnarr[$key]['name'] = $value->name;
-                    $returnarr[$key]['link'] = Auth::user()->hasRole("admin") ? route('requestcheck',["id"=>encrypt($value->id)]) : "#";
+                    // $returnarr[$key]['link'] = Auth::user()->hasRole("admin") ? route('requestcheck',["id"=>encrypt($value->id)]) : "#";
+                    // $returnarr[$key]['link'] = route('requestcheck',["id"=>encrypt($value->id)]);
                 }
                 $req = $returnarr;
             }
--- resources/views/admin/inspector/mail/assign.blade.php
+++ resources/views/admin/inspector/mail/assign.blade.php
@@ -61,10 +61,10 @@
                                                                 <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative;">
                                                                     <tr>
                                                                         
-                                                                        <td style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative;">
+                                                                        <!-- <td style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative;">
                                                                             <a href="https://wind.evergreenbraindev.com/email/verify/5/522eaec705f85ab281a22dad002e366f6074092b?expires=1666439205&amp;signature=f924ce643375c781d7436d0ba4b7119195382ce33a7c839ac02d9020c1a5b528" class="button button-primary" target="_blank" rel="noopener"
                                                                                 style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; -webkit-text-size-adjust: none; border-radius: 4px; color: #fff; display: inline-block; overflow: hidden; text-decoration: none; background-color: #2d3748; border-bottom: 8px solid #2d3748; border-left: 18px solid #2d3748; border-right: 18px solid #2d3748; border-top: 8px solid #2d3748;">Accept</a>
-                                                                        </td>
+                                                                        </td> -->
                                                                     </tr>
                                                                 </table>
                                                             </td>
