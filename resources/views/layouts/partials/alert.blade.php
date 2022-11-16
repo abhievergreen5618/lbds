7297722c99800eeeb293fb8a764fe878270a895b
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2022-11-16 10:55:08 +0530

    update

--- resources/views/layouts/partials/alert.blade.php
+++ resources/views/layouts/partials/alert.blade.php
@@ -7,9 +7,10 @@
 @endif
 {{-- for display error alert --}}
 @if (session()->has('error'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert"
-        @isset($style) {{ $style }} @endisset>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert" @isset($style) {{ $style }} @endisset>
         <p class="m-0">{{ session()->get('error') }}</p>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
+
+
