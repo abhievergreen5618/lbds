b4ed135eff4f5772ba3aac6d7f65cfd74054a26f
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2022-11-21 12:40:21 +0530

    Update profileshow.blade.php

--- resources/views/admin/agency/profileshow.blade.php
+++ resources/views/admin/agency/profileshow.blade.php
@@ -151,7 +151,7 @@
                     <td>{{$value->applicantname}}</td>
                     <td>{{$value->address}}</td>
                     <!-- <td>{{$value->inspectiontype}}</td> -->
-                    <td>{{(!empty($value->schedule_at) ? $value->schedule_at : '')}}</td>
+                    <td>{{(!empty($value->scheduled_at) ? $value->schedule_at : '')}}</td>
                     <td>{{(!empty($value->assigned_ins) ? $value->schedule_at : '')}}</td>
                     <td  class="badge bg-danger">{{$value->status}}</td>
                   </tr>
