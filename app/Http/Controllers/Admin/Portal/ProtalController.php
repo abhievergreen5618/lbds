0c90d981492319cff6e5d18d62aafb49aef2e1de
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2022-11-30 13:08:09 +0530

    Update ProtalController.php

--- app/Http/Controllers/Admin/Portal/ProtalController.php
+++ app/Http/Controllers/Admin/Portal/ProtalController.php
@@ -92,7 +92,6 @@ class ProtalController extends Controller
             $fileName = "";
         }
         $option->updatesetting($values);
-        // $option->envUpdate($request->all());
         return back()->with('msg', 'Website Configuration Updated Successfully');
     }
 
