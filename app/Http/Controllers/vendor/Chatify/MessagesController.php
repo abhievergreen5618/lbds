2f719f01c4bf56abe5c8cd2d4035c2b8c6c6d4b8
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2022-12-08 13:39:57 +0530

    Update MessagesController.php

--- app/Http/Controllers/vendor/Chatify/MessagesController.php
+++ app/Http/Controllers/vendor/Chatify/MessagesController.php
@@ -44,7 +44,6 @@ class MessagesController extends Controller
             return Chatify::pusherAuth(
                 $request['channel_name'],
                 $request['socket_id'],
-                $authData
             );
         }
         // if not authorized
