a2254c2516f8ed8173fefc416ab3cd2faf93a244
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-02-01 12:57:52 +0530

    update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="{{ asset('public/images/black.png') }}" style="max-height: 100% !important; width:100%!important;" class="logo" alt="Laravel Logo" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; max-width: 100%; border: none; height: 75px; max-height: 75px; width: 75px;">
 @else
-<img src="{{asset('public/images/black.png')}}" class="logo" alt="My logo">
+<img src="{{ asset('public/images/black.png') }}" style="max-height: 100% !important; width:100%!important;" class="logo" alt="Laravel Logo" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; max-width: 100%; border: none; height: 75px; max-height: 75px; width: 75px;">
 @endif
 </a>
 </td>
